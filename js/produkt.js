830a23d5aa480cffd2dccb9b92e7d5dd764c5ddd
TroelsAtWork <tras@kea.dk>
2025-02-04 08:27:40 +0100

    Add layout

--- js/produkt.js
+++ js/produkt.js
@@ -1,4 +1,4 @@
-const productId = 1535;
+const productId = 1164; // 1163, 1164
 const productContainer = document.querySelector("#productContainer");
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
@@ -6,20 +6,27 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
 
 function showProduct(data) {
   productContainer.innerHTML = `
-    <figure>
+    <figure class="relative">
         <img
           src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
           alt="Produktbillede"
           class="productImage"
         />
-        <span class="saleLabel">Udsalg!</span>
+        <span class="saleLabel ${!data.discount && "isOnSale"}">
+        -${data.discount}%</span>
       </figure>
       <section class="productDetails">
-        <h2 class="productName">Produktnavn</h2>
+        <h2 class="productName">${data.productdisplayname}</h2>
         <div>
-        <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
-          <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
-          <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
+        <p class="articleType"><span class="bold">Type:</span> ${
+          data.articletype
+        }</p>
+          <p class="productCategory"><span class="bold">Kategori:</span> ${
+            data.category
+          }</p>
+          <p class="productPrice"><span class="bold">Pris:</span> ${
+            data.price
+          },-</p>
         </div>
         <button class="buyButton">Køb nu</button>
       </section>
