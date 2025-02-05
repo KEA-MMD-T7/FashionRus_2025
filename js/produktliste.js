c5c89f22d70da67a9eac4b820ac12a4546270b00
Alan <ale@kea.dk>
2025-02-06 00:24:45 +0100

    ryd op

--- js/produktliste.js
+++ js/produktliste.js
@@ -13,14 +13,8 @@ if (category) {
 
 let data;
 
-let data;
-
 fetch(endpoint)
   .then((response) => response.json())
-  .then((json) => {
-    data = json;
-    showProducts(data);
-  });
   .then((json) => {
     data = json;
     showProducts(data);
